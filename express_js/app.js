a1d89590bd6efbffb87af5a8ce804fb7992187ca
Prathamesh Patil <patilprathameshr@gmail.com>
2023-04-12 01:35:00 +0530

    query and params with express_js

--- express_js/app.js
+++ express_js/app.js
@@ -1,7 +1,7 @@
 const http=require('http')
 const {readFileSync}=require('fs')
 
-const homePage=readFileSync('./html file name')
+const homePage=readFileSync('./index.html')
 
 const server=http.createServer((req,res)=>{
     const url=req.url
